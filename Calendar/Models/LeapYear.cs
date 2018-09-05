edce3d0776673019b32235f4eba3f7e34bc5a62b
SkyeN <skye@dames.es>
2018-09-05 09:10:32 -0700

    add interface

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -1,3 +1,6 @@
+using System;
+using Calendar;
+
 namespace Calendar
 {
     public class LeapYear
@@ -17,5 +20,16 @@ namespace Calendar
                 return year % 4 == 0;
             }
         }
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a year to see if it was/will be a leap year: ");
+        int year = int.Parse(Console.ReadLine());
+        LeapYear myLeapYear = new LeapYear();
+        Console.WriteLine(myLeapYear.IsLeapYear(year));
     }
 }
