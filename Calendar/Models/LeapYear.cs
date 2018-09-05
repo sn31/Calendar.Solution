6b3a308d2a9fdf91bf0b2b762eb1fd3c920e55c6
SkyeN <skye@dames.es>
2018-09-05 05:05:59 -0700

    change IsLeapYear method to pass the fourth test

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -4,7 +4,11 @@ namespace Calendar
     {
         public bool IsLeapYear(int year)
         {
-            if (year % 100 == 0)
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
             {
                 return false;
             }
