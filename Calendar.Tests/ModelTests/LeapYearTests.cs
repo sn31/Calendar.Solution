2f1b5b3b28849fcd64de0e35748a2a6daa539951
SkyeN <skye@dames.es>
2018-09-04 21:49:18 -0700

    add first test method

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,15 @@
+using Calendar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calendar.TestTools
+{
+    [TestClass]
+    public class LeapYearTest
+    {
+        [TestMethod]
+        public void IsLeapYear_NumberDivisibleByFour()
+        {
+            //testing code goes here
+        }
+    }
+}
